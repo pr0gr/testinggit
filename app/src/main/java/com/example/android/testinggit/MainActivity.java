d2c704557bfa9661a4999b7c226a91fbf4f861e3
pr0gr <rrrominus@gmail.com>
2015-11-08 22:28:35 +0300

    commmmmm

--- app/src/main/java/com/example/android/testinggit/MainActivity.java
+++ app/src/main/java/com/example/android/testinggit/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int vara=1000;
+        int varb=200;
 
     }
 }
